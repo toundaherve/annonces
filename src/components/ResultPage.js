42d85998a5bc2b2a95f192711b274868c069ed56
toundaherve <toundaherve@gmail.com>
2021-01-13 22:28:04 +0000

    Add PageSuivante to ResultPage

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -8,8 +8,8 @@ const ResultPage = () => {
         <div className="h5 mb-1 fw-normal">
           Annonces <span className="fw-bold"> 1 - 20 </span>sur{" "}
           <span className="fw-bold">1'662</span> trouve pour :{" "}
-          <span className="fw-bold">"iphone"</span>[
-          <a href="#1">Modifier la recherche</a>]
+          <span className="fw-bold">"iphone"</span>
+          <br />[<a href="#1">Modifier la recherche</a>]
         </div>
 
         <div>
@@ -26,7 +26,9 @@ const ResultPage = () => {
               {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((n) => {
                 return (
                   <tr key={n}>
-                    <th scope="row">Iphone 8 plus - 64GB ...</th>
+                    <th className="fw-normal" scope="row">
+                      Iphone 8 plus - 64GB ...
+                    </th>
                     <td>$750</td>
                     <td>Leeds, UK</td>
                     <td>45 min</td>
@@ -35,6 +37,30 @@ const ResultPage = () => {
               })}
             </tbody>
           </table>
+        </div>
+
+        <div className="mt-4">
+          <div className="row row-cols-auto justify-content-between">
+            <div className="col">
+              {[1, 2, 3, 4, 5].map((n) => (
+                <a className="p-2" key={n} href="#4">
+                  {n}
+                </a>
+              ))}
+              <span className="p-2" href="#4">
+                ...
+              </span>
+              <a className="p-2" href="#4">
+                {84}
+              </a>
+            </div>
+
+            <div className="col">
+              <a className="p-2" href="#4">
+                page suivante <span>>></span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
